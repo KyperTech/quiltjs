72a86cf22c4d177d68873ece4ef7b8f6386969f8
mvanlonden <melvanlonden@gmail.com>
2014-01-14 20:49:47 -0500

    create dynamic row and column system

--- grid.js
+++ grid.js
@@ -1,46 +1,55 @@
+function createDiv(type, className, id, width, height) {
+	var el = document.createElement(type);
+	el.className = className;
+	el.id = id;
+	el.style.width = width;
+	el.style.height = height;
+	return el;
+};
+
 var cell = {
 	width: 30,
 	height: 20,
-	className: 'cell',
-	createCell: function(){
-		var cellEl = document.createElement('div');
-		cellEl.className = this.className;
-		cellEl.style.width = this.width;
-		cellEl.style.height = this.height;
-		return cellEl
+	createCell: function(id){
+		var cellEl = createDiv('div', 'cell', id, this.width, this.height);
+		return cellEl;
+	}
+};
+
+var row = {
+	width: function(){
+		return cell.width * grid.columns;
+	},
+	createRow: function(id){
+		console.log(this.width());
+		var rowEl = createDiv('div', 'row', id, this.width(), cell.height);
+		for(var i = 0; i < grid.columns; i++){
+			rowEl.appendChild(cell.createCell(i));
+		}
+		return rowEl;
 	}
 };
 
 var grid = {
 	rows: 30,
-	columns: 30,
+	columns: 15,
 	height: function(){
-		return this.rows * cell.height
+		return this.rows * cell.height;
 	},
 	width: function(){
-		return this.columns * cell.width
+		return this.columns * cell.width;
 	},
-	className: 'grid',
-	gridEl: null,
 	createGridDiv: function(){
-		var gridEl = document.createElement('div');
-		gridEl.className = this.className;
-        gridEl.style.width = this.width();
-        gridEl.style.height = this.height();
-        this.gridEl = gridEl;
-	},
-	createRows: function(){
-
-	},
-	stackCells: function(){
-		for(var i = 0; i < (this.rows * this.columns); i++){
-			this.gridEl.appendChild(cell.createCell());
-		}
+        var gridContainEl = createDiv('div', 'grid', null, this.width(), this.height());
+        return gridContainEl;
 	},
 	addToDoc: function(){
-		document.getElementById('container').appendChild(this.gridEl);
+		var gridEl = grid.createGridDiv();
+		for(var i = 0; i < this.columns; i++){
+			gridEl.appendChild(row.createRow(i));
+		}
+		document.getElementById('container').appendChild(gridEl);
 	}
 };
-grid.createGridDiv();
-grid.stackCells();
+
 grid.addToDoc();
