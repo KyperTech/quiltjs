a43e683f5d6310c06fe740e7dca11036f4b90d23
mvanlonden <melvanlonden@gmail.com>
2014-01-16 07:27:37 -0500

    grid infrastructure with button resize test'

--- grid.js
+++ grid.js
@@ -1,18 +1,25 @@
-function createDiv(type, className, id, width, height) {
-	var el = document.createElement(type);
-	el.className = className;
-	el.id = id;
-	el.style.width = width;
-	el.style.height = height;
+function Div(className, width, height, id){
+	this.width = width;
+	this.height = height;
+	this.className = className;
+	this.id = id;
+};
+
+Div.prototype.create = function(id){
+	var el = document.createElement('div');
+	el.className = this.className;
+	el.id = this.id || (isNaN(id) ? null: id);
+	el.style.width = this.width;
+	el.style.height = this.height;
+	if(isNaN(this.id)){this.id = (isNaN(id) ? null: id)};
 	return el;
 };
 
 var cell = {
 	width: 30,
 	height: 20,
-	createCell: function(id){
-		var cellEl = createDiv('div', 'cell', id, this.width, this.height);
-		return cellEl;
+	el: function(){
+		return new Div('cell', this.width, this.height)
 	}
 };
 
@@ -20,13 +27,11 @@ var row = {
 	width: function(){
 		return cell.width * grid.columns;
 	},
-	createRow: function(id){
-		console.log(this.width());
-		var rowEl = createDiv('div', 'row', id, this.width(), cell.height);
-		for(var i = 0; i < grid.columns; i++){
-			rowEl.appendChild(cell.createCell(i));
-		}
-		return rowEl;
+	height: function(){
+		return cell.height;
+	},
+	el: function(){
+		return new Div('row', this.width(), this.height())
 	}
 };
 
@@ -34,22 +39,41 @@ var grid = {
 	rows: 30,
 	columns: 15,
 	height: function(){
-		return this.rows * cell.height;
+		return cell.height * this.rows;
 	},
 	width: function(){
-		return this.columns * cell.width;
+		return cell.width * this.columns;
 	},
-	createGridDiv: function(){
-        var gridContainEl = createDiv('div', 'grid', null, this.width(), this.height());
-        return gridContainEl;
+	el: function(){
+		return new Div(null, this.width(), this.height(), 'grid')
 	},
 	addToDoc: function(){
-		var gridEl = grid.createGridDiv();
-		for(var i = 0; i < this.columns; i++){
-			gridEl.appendChild(row.createRow(i));
+		var gridEl = this.el().create();
+		for(var i = 1; i <= this.rows; i++){
+			var rowEl = row.el().create(i);
+		    for(var j = 1; j <= this.columns; j++){
+		    	rowEl.appendChild(cell.el().create(j))
+		    }
+		    gridEl.appendChild(rowEl);
 		}
 		document.getElementById('container').appendChild(gridEl);
+	},
+	resize: function(width, height){
+		cell.width = width;
+		cell.height = height;
+		var grid = document.getElementById('grid');
+		grid.style.width = this.width();
+		grid.style.height = this.height();
+		var rows = document.getElementsByClassName('row');
+		for(var i = 0; i < rows.length; i++){
+			rows[i].style.width = row.width();
+			rows[i].style.height = row.height();
+		}
+		var cells = document.getElementsByClassName('cell');
+		for(var i = 0; i < cells.length; i++){
+     		cells[i].style.width = cell.width;
+     		cells[i].style.height = cell.height;
+  		}
 	}
 };
-
 grid.addToDoc();
